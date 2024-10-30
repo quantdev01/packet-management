f48396010c2ba7bf0c0c8a44172e08e8d95b857b
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-10-30 22:22:16 +0200

    Okay with all

--- lib/screens/add_packet.dart
+++ lib/screens/add_packet.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_build_context_synchronously
+// ignore_for_file: use_build_context_synchronously, depend_on_referenced_packages, no_logic_in_create_state
 
 import 'dart:developer';
 
